61c7d98e28b9584da5895a1c2e5fd43d749b91ec
Andrey Rubtsov <aa.rubtsov@icloud.com>
2024-11-14 15:17:47 +0300

    finak v1.0\

--- script.js
+++ script.js
@@ -1,48 +1,53 @@
-let elBtnGetContact = document.querySelector(".btn_contact");
-
-elBtnGetContact.addEventListener("click", () => {
-  getContacts();
-});
-
-let elBtnGetTasks = document.querySelector(".btn_tasks");
-
-elBtnGetTasks.addEventListener("click", () => {
-  getTasks();
-});
-
-
 let limit = 1;
 
 let page = 1;
 
-const HOST = "https://introverttesttask2.amocrm.ru";
+let timeTaskEnd = 30 // в днях
 
-let apiRoutes = {
-  contacts: "/api/v4/contacts?order[id]=asc",  // наверное, выборку лучше делать с сортировкой, что бы не задублировать одинаковые контакты на разных страницах
-  tasks: "/api/v4/tasks",
+const API = {
+  amocrm: {
+    host: "https://introverttestapi.amocrm.ru",
+    routes: {
+        contacts: "/api/v4/contacts?order[id]=asc",  // наверное, выборку лучше делать с сортировкой, что бы не задублировать одинаковые контакты на разных страницах
+        tasks: "/api/v4/tasks",
+    }
+  }
 };
 
 let listMessages = {
   get_error_contacts: {
-    tag: "json_contact",
+    tag: "#contact",
     messages: "Что-то пошло не так c получением контактов.",
   },
   no_contacts: {
-    tag: "json_contact",
-    messages: "В базе больше нет контактов.",
+    tag: "#contact",
+    messages: "Все контакты обработаны.",
   },
   get_error_tasks: {
-    tag: "json_tasks",
+    tag: "#task",
     messages: "Что-то пошло не так c загрузкой задач.",
   },
+  tasks_created:{
+    tag: "#task",
+    messages: "Задачи созданы.",
+  },
+  no_tasks_created:{
+    tag: "#task",
+    messages: "Контактов для создания задач нет.",
+  },
+  other: {
+    no_callback: "Забыли передать в запрос callback"
+  },
+
 };
 
 const taskLabel = "Контакт без сделок";
 
 const TOKEN_AMO =
-  "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjU0NmMzNGJmN2IyYzFjYzBjOTg2MTY5Y2NhMmQ2Y2EwY2MzMzJiZDc0YmRkZjAzZTg1Y2VlYzU5ZmY4ODY2NTI4NmQxMzE3ZTdlMjdhOWFkIn0.eyJhdWQiOiI3ZDE2NDM1NS1mNmM3LTQwZTgtOTY5ZS02MGNmMzdmZTg3OGQiLCJqdGkiOiI1NDZjMzRiZjdiMmMxY2MwYzk4NjE2OWNjYTJkNmNhMGNjMzMyYmQ3NGJkZGYwM2U4NWNlZWM1OWZmODg2NjUyODZkMTMxN2U3ZTI3YTlhZCIsImlhdCI6MTczMTUwNzA2MSwibmJmIjoxNzMxNTA3MDYxLCJleHAiOjE3NjE5NTUyMDAsInN1YiI6IjExNzU2MTMwIiwiZ3JhbnRfdHlwZSI6IiIsImFjY291bnRfaWQiOjMyMDY0ODk0LCJiYXNlX2RvbWFpbiI6ImFtb2NybS5ydSIsInZlcnNpb24iOjIsInNjb3BlcyI6WyJjcm0iLCJmaWxlcyIsImZpbGVzX2RlbGV0ZSIsIm5vdGlmaWNhdGlvbnMiLCJwdXNoX25vdGlmaWNhdGlvbnMiXSwiaGFzaF91dWlkIjoiZGY4ZTE0NjYtMTMxNi00YzUwLTk3MGUtMGJjZDcyYjQyNjRkIiwiYXBpX2RvbWFpbiI6ImFwaS1iLmFtb2NybS5ydSJ9.MDBmnsoDH6nUexMWCo5cwAdSnhZtuPcSuipQ3F0SD8bDYX52uJdl-FEpyxcGpXAouShGOcu67Wz1ioVQnu5E-9AXx0wtURjFVhKOx30xPIKJoaOJDflc8SLM9-w49o3sWiSl3ixqCG5DoLA7Erz1Au5f7gWItlFlDICpqZZ9oZZSQ5bZ2LlM8q6BRJ67nmEAVndNYr5c0yD0YmcB1lnl1y3IhMB2nCKWbIVcQE-eJGxDCWLmtk6qmDzH0eAnle0yGd7iX7yMQuQUmsplmti03kpeIGmLt0ywqh3QxgLEtxjPuY-fb-oumM5pIaQJ4EitdyF13-j_WE4r4y7PjweG_A";
+  "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImEwMTgxMjA0NWZmZDFmZTQ3ZjhlZGE2MTA1MDZjMjY4YTQzNjZmMGQ2NjI0ZmM4Nzk5ZjBiYTI0MmQzZTdhYmU3MTAyMThkMjc5OWZjNDgzIn0.eyJhdWQiOiJlNjc3ZTU3OC0wOWU5LTQzZDItOGE5OC0wMTlkOTI0NmU0MjUiLCJqdGkiOiJhMDE4MTIwNDVmZmQxZmU0N2Y4ZWRhNjEwNTA2YzI2OGE0MzY2ZjBkNjYyNGZjODc5OWYwYmEyNDJkM2U3YWJlNzEwMjE4ZDI3OTlmYzQ4MyIsImlhdCI6MTczMTU2NDM5NCwibmJmIjoxNzMxNTY0Mzk0LCJleHAiOjE3NjcyMjU2MDAsInN1YiI6IjExNzU2MTMwIiwiZ3JhbnRfdHlwZSI6IiIsImFjY291bnRfaWQiOjMyMDY1ODU4LCJiYXNlX2RvbWFpbiI6ImFtb2NybS5ydSIsInZlcnNpb24iOjIsInNjb3BlcyI6WyJjcm0iLCJmaWxlcyIsImZpbGVzX2RlbGV0ZSIsIm5vdGlmaWNhdGlvbnMiLCJwdXNoX25vdGlmaWNhdGlvbnMiXSwiaGFzaF91dWlkIjoiM2FjOTk4NTYtZWI1YS00NGI5LWIwNTQtMGZjYzY1ODcwMWJlIiwiYXBpX2RvbWFpbiI6ImFwaS1iLmFtb2NybS5ydSJ9.at3ZV4sJjeCDGmqcnakmXXXvsEUua1lsryDC707qmAeHwvsbxrC_bqB0AC8l20hu4AJ-Z_Z53ki1lE-qLExmSmjmHV4tZpPoo97fuDv1e4UyzptnmLMwnhu5CtnYkPjf39PrVBFFkZltIV_OM7vyg8OAOWD-r987o-zKOwY5Tfem5cW04ZdjQ87A4W8Nd2HeCoUWkATVSTjzvcz0-9r69-qLzkWq8YHRnP0li2maSqLDdkzh_bQ2q794T7pU3mNrqKi8fz8Gk9YX8_vjp1TvuNzZADLn1cD6b3AppDpXCGsFn62Z5f8YfpitXmHTOVURjS9GplaniPuvgSv1GbL9nw";
 
 
+// Шаблон для запроса на API
 function request(
   params = {
     url: "",
@@ -53,38 +58,40 @@ function request(
 ) {
   $.ajax({
     crossDomain: true,
-    url: HOST + params.url,
+    url: API.amocrm.host + params.url,
     method: params.metod,
     data: params.data,
     dataType: "json",
     headers: {
       Authorization: "Bearer" + " " + TOKEN_AMO,
-      // contentType: "application/json",
+      contentType: "application/json",
     },
   })
     .done(function (data, jqXHR) {
       params.callback ?
         params.callback(data, jqXHR)
         :
-        console.log("Забыли передать в запрос callback");
+        console.log(listMessages.other.no_callback);
     })
     .fail(function (error, textStatus) {
       params.callback ?
         params.callback(error, textStatus)
         :
-        console.log("Забыли передать в запрос callback");
+        console.log(listMessages.other.no_callback);
       return false;
     });
 }
 
+// Вывод сообщений
 function outputMessage(tag, messages){
   const dataString = JSON.stringify(messages, null, "  ");
-  document.getElementById(tag).innerHTML = dataString;
+  $(tag).text(dataString);
 }
+
 
 function getContacts() {
   request({
-    url: apiRoutes.contacts,
+    url: API.amocrm.routes.contacts,
     metod: "GET",
     data: {
       limit: limit,
@@ -111,8 +118,6 @@ function getContacts() {
         return;
       }
 
-      console.log('getContacts>>>', response, status);
-
       if(!!response._embedded.contacts){
         let contacts = response._embedded.contacts
 
@@ -122,13 +127,14 @@ function getContacts() {
 
         page++;
       }
+
       getContacts();  // рекурсивно пробегаемся по всем страницам контактов
     },
   });
 }
 
+
 function checkLeads(listContacts){
-  console.log('checkLeads>>>', listContacts);
 
   let idContactsNoLeads = []
 
@@ -141,15 +147,16 @@ function checkLeads(listContacts){
   return idContactsNoLeads;
 }
 
+
 function getTasks(contactsWithoutLeads){
-  console.log('checkDoubleTasks>>>', contactsWithoutLeads);
+
   let filterApiTask = "?filter[task_type]=1&filter[is_completed]=0&filter[entity_type]=contacts";
 
   request({
-    url: apiRoutes.tasks + filterApiTask,
+    url: API.amocrm.routes.tasks + filterApiTask,
     metod: "GET",
     callback: (response, status) => {
-      console.log('getTasks>>>', response, status, response);
+      let listContacts = contactsWithoutLeads;
 
       if(status === "error") {
         outputMessage(
@@ -164,17 +171,28 @@ function getTasks(contactsWithoutLeads){
         return;
       }
 
+      let listTasks = response._embedded.tasks;
 
+      listTasks.forEach(task =>{
+        if(task.text == taskLabel){
+          let indexContactDel = contactsWithoutLeads.indexOf(task.entity_id);
+          if (indexContactDel != -1) {
+            listContacts.splice(indexContactDel, 1);
+          }
+        };
+      });
 
+      createTasks(contactsWithoutLeads);
     },
   });
-
 }
 
+
 function createTasks(listIdContactsNoLeads){
-  console.log('createTasks>>', listIdContactsNoLeads);
 
   let packageLeads = [];
+
+  let complete_task = Math.floor(Date.now() / 1000 + timeTaskEnd * 86400);
 
   listIdContactsNoLeads.forEach(idContact => {
     packageLeads.push(
@@ -183,36 +201,36 @@ function createTasks(listIdContactsNoLeads){
         text: taskLabel,
         entity_id: idContact,
         entity_type: "contacts",
-        "complete_till": 1588885140,
+        complete_till: complete_task,
       }
     );
   });
 
-  request({
-    url: apiRoutes.tasks,
-    metod: "POST",
-    data: JSON.stringify(packageLeads),
-    callback: (response, status) => {
-      console.log('создание задачь >>>', response, status);
-    },
-  });
+  if (packageLeads.length){
+    request({
+      url: API.amocrm.routes.tasks,
+      metod: "POST",
+      data: JSON.stringify(packageLeads),
 
+      callback: (response) => {
+        console.log('ЗАДАЧИ СОЗДАНЫ >>>', response._embedded.tasks);
+
+        outputMessage(
+          listMessages.tasks_created.tag,
+          listMessages.tasks_created.messages
+        );
+      },
+    });
+  };
+
+  outputMessage(
+    listMessages.no_tasks_created.tag,
+    listMessages.no_tasks_created.messages,
+  );
 }
 
-// function getTasks() {
 
-//   request({
-//     url: "/api/v4/tasks",
-//     metod: "GET",
-//     data: {
-//       limit: limit,
-//       page: page,
-//     },
-//     callback: (response) => {
-//       console.log(response);
-//     },
-//   });
-//   page++;
-
-// }
-// checkDoubleTasks
+$(".btn_contact").on("click", function() {
+  $(this).attr("disabled", true);
+  getContacts();
+});
